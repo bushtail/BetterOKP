2e27495bf502babee82ec06250f37d3563162aa6
bushtail <evannosich@icloud.com>
2026-03-03 17:18:51 -0500

    b

--- BetterOKP/BetterOKP.cs
+++ BetterOKP/BetterOKP.cs
@@ -1,5 +1,22 @@
-﻿namespace BetterOKP;
+﻿using JetBrains.Annotations;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+using SPTarkov.Server.Core.Services;
 
-public class Class1
+namespace BetterOKP;
+
+[Injectable(TypePriority = OnLoadOrder.TraderRegistration - 1), UsedImplicitly]
+public class BetterOKP(DatabaseService databaseService) : IOnLoad
 {
+    public Task OnLoad()
+    {
+        var items = databaseService.GetTables().Templates.Items;
+        items[ItemTpl.COLLIMATOR_OKP7_REFLEX_SIGHT].Properties!.Prefab = new Prefab
+        {
+            Path = "scope_all_ekb_okp7_chevron.bundle",
+            Rcid = ""
+        };
+        return Task.CompletedTask;
+    }
 }
