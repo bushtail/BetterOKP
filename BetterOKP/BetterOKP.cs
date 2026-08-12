50050ac19a555869f3fdaa68ddc9fc38a628df07
bushtail <evannosich@icloud.com>
2026-08-12 00:34:51 -0400

    4.1.0

--- BetterOKP/BetterOKP.cs
+++ BetterOKP/BetterOKP.cs
@@ -2,16 +2,18 @@
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
 using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+using SPTarkov.Server.Core.Models.Enums;
+using SPTarkov.Server.Core.Models.Spt.Tables;
 using SPTarkov.Server.Core.Services;
 
 namespace BetterOKP;
 
 [Injectable(TypePriority = OnLoadOrder.TraderRegistration - 1), UsedImplicitly]
-public class BetterOKP(DatabaseService databaseService) : IOnLoad
+public class BetterOKP(TemplateTable templateTable) : IOnLoad
 {
-    public Task OnLoad()
+    public Task OnLoadAsync(CancellationToken cancellationToken)
     {
-        var items = databaseService.GetTables().Templates.Items;
+        var items = templateTable.Items;
         items[ItemTpl.COLLIMATOR_OKP7_REFLEX_SIGHT].Properties!.Prefab = new Prefab
         {
             Path = "scope_all_ekb_okp7_chevron.bundle",
